9e3cc1ced297db8e93e02f5d689aa71d1e267f64
gsotoc <germansoto7878@gmail.com>
2025-06-23 21:32:47 -0300

    landing page completa

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,30 @@ import './App.css'
 function App() {
   const [isCartOpen, setIsCartOpen] = useState(false); //Estado para saber si el carrito está cerrado o abierto
   const [cartItems, setCartItems] = useState([]); //Estado para agregar los productos del carrito
+  const count = 0;
 
+ const increaseCount = (id) => {
+    const updatedCart = cartItems.map((item) =>
+      item.id === id ? { ...item, count: item.count + 1 } : item
+    );
+    setCartItems(updatedCart);
+  };
+
+  const decreaseCount = (id) => {
+    const updatedCart = cartItems.map((item) =>
+      item.id === id && item.count > 1 ? { ...item, count: item.count - 1 } : item
+    );
+    setCartItems(updatedCart);
+  };
+
+  
   const addToCart = (producto) => {
-    setCartItems([...cartItems, producto]);
+    
+    if (cartItems.find((item) => item.id === producto.id)) {
+      increaseCount(producto.id);
+    } else {
+      setCartItems([...cartItems, { ...producto, count: 1 }]);
+    }
   };
 
   const removeFromCart = (id) => {
@@ -23,6 +44,8 @@ function App() {
         setIsCartOpen={setIsCartOpen}
         cartItems={cartItems}
         removeFromCart={removeFromCart}
+        increaseCount={increaseCount}
+        decreaseCount={decreaseCount}
       />
 
       <ItemListContainer addToCart={addToCart}/>
--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,18 +1,28 @@
-function CartWidget({ items, onClose, removeFromCart }) {
-
+function CartWidget({ items, onClose, removeFromCart, increaseCount, decreaseCount }) {
   return (
     <aside>
       <h2>Carrito</h2>
       {items.length === 0 ? (
         <p>No hay productos en el carrito.</p>
       ) : (
-        <ul>
+        <div>
           {items.map((item) => (
-            <li className="cartItem" key={item.id}>
-              {item.title} - ${item.price} <button className="removeItem" onClick={() => removeFromCart(item.id)}>x</button>
-            </li>
+            <article className="cartItem" key={item.id}>
+              <header>
+                {item.title}
+                <button className="removeItem" onClick={() => removeFromCart(item.id)}>x</button>
+              </header>
+              <div className="cartItemCount">
+                <button onClick={() => decreaseCount(item.id)} disabled={item.count <= 1}>-</button>
+                <p>{item.count}</p>
+                <button onClick={() => increaseCount(item.id)}>+</button>
+              </div>
+              <div className="cartItemPrice">
+                <p>Total: $ {(item.price * item.count).toFixed(2)}</p>
+              </div>  
+            </article>
           ))}
-        </ul>
+        </div>
       )}
       <button onClick={onClose}>Cerrar</button>
     </aside>
